d3fdac270f7146b28f5f853d7c7f7e8a45d3c40a
Samrat-GC <61535011+Samrat-GC@users.noreply.github.com>
2020-12-20 20:11:25 +0545

    initial commit

---
+++ script/app.js
@@ -0,0 +1,85 @@
+let chatRoom = document.querySelector('.chat-rooms');
+const chat = document.querySelector('.new-chat');
+const user = document.querySelector('.new-name');
+const chatWindow = document.querySelector('.chat-window .chat-list');
+const update_name = document.querySelector('.update_name');
+
+
+// Check for the saved username in localstorage if any
+const username = localStorage.username ? localStorage.username : 'user';
+
+// Listen for the room user click on and display the chat according to activeChatRoom
+chatRoom.addEventListener('click', e => {
+    if(e.target.id){
+				activeChatRoom = e.target.id;
+				display.wipe();
+        newStuff.updateRoom(activeChatRoom);
+				newStuff.getChats(data => display.display(data));
+				// setTimeout(()=>chatWindow.lastElementChild.scrollIntoView(),300)
+				
+
+    }
+})
+
+// if user updates name listen for it and change the username in ChatRoom class 
+user.addEventListener('submit', (e) => {
+    e.preventDefault();
+    userName = user.name.value.trim();
+    newStuff.updateName(userName);
+    update_name.textContent = `Your name has been updated to ${userName}`;
+    setTimeout(()=>{
+        update_name.textContent = null;
+    }, 3000);
+
+});
+
+// post the added chat in database
+chat.addEventListener('submit', e => {
+    if(chat.message.value){
+    e.preventDefault();
+    newStuff.addChat(chat.message.value);
+    
+}
+})
+
+// create new instance of DisplayChat giving it the argument of ul 
+const display = new DisplayChat(chatWindow);
+
+// create new instance of Chatroom and default the room to general. Username depends on weather there is pre-defined usename in local storage or not.
+const newStuff = new Chatroom('general', username);
+
+// Get all the chats and actively listen to any kind of changes from the database
+newStuff.getChats(data => display.display(data));
+// setTimeout(()=>chatWindow.lastElementChild.scrollIntoView(),300)c
+
+// window.setTimeout(()=> {
+// 	chatWindow.lastElementChild.scrollIntoView()}, 3000);
+chatWindow.addEventListener('click', e => {
+	if(e.target.tagName === 'LI'){
+		console.log(e.target.id);
+}
+});
+
+
+	// display in browser majjale
+// const displayf =  (item, id) => {
+//     const mes = item.message;
+//     samay = item.created_at.toDate();
+//     const usernaam = item.username;
+    
+//     const html = `
+//     <li>
+//     ${mes}
+//     <br>
+//     <span class='small_text'>${samay}</span>
+//     <span class='small_text'>${usernaam}</span>
+//     </li>
+//     `;
+//     display.innerHTML += html;
+// }
+
+// db.collection('chats')
+// .where('room', '==')
+// .onSnapshot(snapshot => {
+//     snapshot.docChanges().forEach(change=>)
+// })
---
+++ script/chat.js
@@ -0,0 +1,55 @@
+class Chatroom {
+    constructor(room, username){
+        this.room = room;
+        this.username = username;
+        this.chats = db.collection('chats');
+				this.unsub;
+				// this.chatlist = chatlist;
+    }
+    async addChat(message){
+        // format a chat object
+        const now = new Date();
+        const chat = {
+            created_at: firebase.firestore.Timestamp.fromDate(now),
+            message, 
+            username: this.username,
+            room: this.room
+				};
+				
+				// this.chats.doc().set(chat);
+        // save the chat documents
+        const response = await this.chats.add(chat);
+        return response;
+
+    }
+    async getChats(callback){
+         this.unsub = await this.chats
+        .where('room', '==', this.room)
+        .orderBy('created_at')
+            .onSnapshot(snapshot => {
+                snapshot.docChanges().forEach(change => {
+                    if(change.type === 'added'){
+                        // update the UI
+                        callback(change.doc);
+
+                    }else if(change.type === 'removed'){
+                        
+                    }
+								})
+
+				})
+			};
+
+
+				// document.querySelector('.chat_list').lastElementChild.scrollIntoView();
+						
+  
+    updateName(username){
+				this.username = username;
+				localStorage.setItem('username', username);
+    }
+    updateRoom(room){
+				this.room = room;
+				this.unsub();
+    }
+}
